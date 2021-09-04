041d4dee89e075314c2c8b981467c04a0c3ca63c
cesaravalos <cesar_avalos2@yahoo.com.ar>
2021-09-03 22:42:58 -0300

    first commit

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "¿Quién fue el primer gobernador del Chaco?",
+    answer: "Felipe Gallardo",
+    options: [
+      "Florencio Tenev",
+      "Felipe Gallardo",
+      "Leolindo F. Bittel",
+      "Angel Rozas"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Quién es el patrono de Resistencia?",
+    answer: "San Fernando Rey",
+    options: [
+      "San Fernando Rey",
+      "El Cura Brochero",
+      "San Juan Bautista",
+      "Virgen de Itatí"
+    ]
+  },
+    {
+    numb: 3,
+    question: " ¿Cómo es considerada la ciudad de Castelli?",
+    answer: "Portal del Impenetrable",
+    options: [
+      "Portal del Impenetrable",
+      "Perla del Paraná",
+      "Ciudad de las Esculturas",
+      "Ciudad de la Cerámica"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Cuáles es el relieve caractérístico del Chaco?",
+    answer: "Llanura",
+    options: [
+      "Llanura",
+      "Meseta",
+      "Montaña",
+      "Sierra"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Cuál es la flor provincial del Chaco?",
+    answer: "Flor rosada del palo borracho",
+    options: [
+      "Flor del cardo",
+      "Flor del Ceibo",
+      "Flor rosada del palo borracho",
+      "Flor del Camalote"
+    ]
+  },
+  {
+    numb: 6,
+    question: "¿En qué año se terminó de construir El Puente General Belgrano?",
+    answer: "1974",
+    options: [
+      "1971",
+      "1974",
+      "1984",
+      "1989"
+    ]
+  },
+  {
+    numb: 7,
+    question: "¿Desde cuándo existe la Provincia del Chaco?",
+    answer: "8 de Agosto de 1951",
+    options: [
+      "8 de Agosto de 1951",
+      "8 de Agosto de 1955",
+      "8 de Agosto de 1961",
+      "10 de Octubre de 1964"
+    ]
+  },
+  {
+    numb: 8,
+    question: "La Fiesta Nacional del Algodón se desarrolla en ",
+    answer: "Presiden",
+    options: [
+      "Charata",
+      "Resistencia",
+      "Gancedo",
+      "Presidencia Roque Sáenz Peña"
+    ]
+  },
+  {
+    numb: 9,
+    question: "¿Cuál de las siguientes etnias NO son poblaciones originarias del Chaco ?",
+    answer: "Huarpes",
+    options: [
+      "Qom",
+      "Moqoi",
+      "Wichi",
+      "Huarpes"
+    ]
+  },
+  {
+    numb: 10,
+    question: "A inicios del siglo XX, la actividad económica más importante del territorio era ",
+    answer: "Algodón",
+    options: [
+      "Tanino",
+      "Algodón",
+      "Pesca",
+      "Caza"
+    ]
+  },
+ 
+];
